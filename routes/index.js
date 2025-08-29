b0ac5c1be601c3f0a9cb6ac1e71635657b5945ab
sai-dabbiru <149162359+sai-dabbiru@users.noreply.github.com>
2025-08-29 15:35:22 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express12' });
+  res.render('index', { title: 'benevity-demo' });
 });
 
 module.exports = router;
